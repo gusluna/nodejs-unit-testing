fce5a098dc5191878473c386839bf91bb8b4ba84
Gustavo Luna <gluna@Gustavos-MacBook-Pro.local>
2020-06-10 14:33:57 -0500

    Before and After

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,21 @@ const assert = require("assert");
 
 describe("file to be tested", () => {
   context("function to be tested", () => {
+    before(() => {
+      console.log("======== before ======");
+    });
+
+    after(() => {
+      console.log("======== after ======");
+    });
+
+    beforeEach(() => {
+      console.log("======== before Each ======");
+    });
+
+    afterEach(() => {
+      console.log("======== after Each ======");
+    });
     it("should do something", () => {
       assert.equal(1, 1);
     });
